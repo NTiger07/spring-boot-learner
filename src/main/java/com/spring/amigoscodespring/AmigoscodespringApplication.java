25e13c88a8f24e441f849f5d8ce585d1a9fd2888
NTiger07 <falaleru@gmail.com>
2023-12-08 14:25:17 +0100

    Initial Commit

---
+++ src/main/java/com/spring/amigoscodespring/AmigoscodespringApplication.java
@@ -0,0 +1,13 @@
+package com.spring.amigoscodespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmigoscodespringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmigoscodespringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/amigoscodespring/AmigoscodespringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.amigoscodespring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmigoscodespringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
