e83c80ca0e126ea8277802571dfe1f0a53517d92
NTiger07 <falaleru@gmail.com>
2024-01-04 02:54:18 +0100

    SetUp Student Class

--- src/main/java/com/spring/amigoscodespring/AmigoscodespringApplication.java
+++ src/main/java/com/spring/amigoscodespring/AmigoscodespringApplication.java
@@ -1,13 +1,39 @@
 package com.spring.amigoscodespring;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.Month;
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.spring.amigoscodespring.student.Student;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
 
 @SpringBootApplication
+@RestController
 public class AmigoscodespringApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AmigoscodespringApplication.class, args);
+		System.out.println("Server Started ");
+	}
+
+	@GetMapping
+	public List<Student> hello() {
+		return List.of(
+			new Student (
+				1L, 
+				"Favour", 
+				"favourtesting@gmail.com", 
+				LocalDate.of(2005, Month.SEPTEMBER, 24), 
+				21
+				)
+			);
 	}
 
 }
